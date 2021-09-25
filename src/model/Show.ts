2d44da685d166f4fdd44ee942ec9b11f6e3a38d9
danilo-ac <anthony.danilo@outlook.com.br>
2021-09-25 20:54:55 -0300

    :arrow_up: Show review and adjustment

--- src/model/Show.ts
+++ src/model/Show.ts
@@ -6,9 +6,36 @@ export interface createShowDTO {
     showId?: string
 }
 
-export type showsAgenda = {
+export class ShowsAgenda {
+    constructor(
+        public name: string,
+        public musicGenre: string,
+        public startTime: number,
+        public endTime: number
+    ) { }
+
+    static toShowsAgendaModel(input: any) {
+        return new ShowsAgenda(
+            input.name,
+            input.musicGenre,
+            input.startTime,
+            input.endTime
+        )
+    }
+}
+
+export interface resultDatabaseModel {
+    showId: string,
     name: string,
+    bandId: string,
+    weekDay: string,
     musicGenre: string,
     startTime: number,
     endTime: number
-  }
+}
+
+export enum ShowWeekDay {
+    SEXTA = "sexta",
+    SABADO = "sábado",
+    DOMINGO = "domingo"
+}
