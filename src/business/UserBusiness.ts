85feae7bd61a7151f71cefa1bb34e31d5bcf581a
danilo-ac <anthony.danilo@outlook.com.br>
2021-09-26 17:38:51 -0300

    :arrow_up: User endpoint and tests| review and ajustments

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -4,37 +4,78 @@ import { IdGenerator } from "../services/IdGenerator";
 import { HashManager } from "../services/HashManager";
 import { Authenticator } from "../services/Authenticator";
 import { UserDataBaseMock } from "../../tests/mocks/userDataBase";
+import EmailValidation from "../services/EmailValidation";
 
 export class UserBusiness {
-    constructor(private userDataBase:UserDatabase ){}
-    async createUser(user: UserInputDTO) {
-       
-        if(!user.email){
-            throw new Error("Email vazio")
-        }
-        const idGenerator = new IdGenerator();
-        const id = idGenerator.generate();
+    constructor(
+        private userDataBase: UserDatabase,
+        private authenticator: Authenticator,
+        private hashManager: HashManager,
+        private idGenerator: IdGenerator,
+        private emailValidation: EmailValidation
+    ) { }
 
-        const hashManager = new HashManager();
-        const hashPassword = await hashManager.hash(user.password);
+
+    async createUser(user: UserInputDTO) {
+
+        if (!user.email) {
+            throw new Error("Empty 'Email'")
+        } else if (
+            !this.emailValidation.isValidEmail(user.email)
+        ) {
+            throw new Error("Invalid 'Email'")
+        }
+
+        if (!user.password) {
+            throw new Error("Empty 'Password'")
+        }
+
+        if (!user.name) {
+            throw new Error("Empty 'Name'")
+        }
+
+        if (!user.role) {
+            throw new Error("Empty 'Role'")
+        } else if (!["ADMIN", "NORMAL"]
+            .includes(user.role.toLocaleUpperCase().trim())) {
+            throw new Error("'Role' must be 'admin' or 'normal'")
+        }
+
+
+        const id = this.idGenerator.generate();
+
+        user.role = user.role.toUpperCase().trim()
+
+        const hashPassword = await this.hashManager.hash(user.password);
         await this.userDataBase.createUser(id, user.email, user.name, hashPassword, user.role);
 
-        const authenticator = new Authenticator();
-        const accessToken = authenticator.generateToken({ id, role: user.role });
+
+        const accessToken = this.authenticator.generateToken({ id, role: user.role });
 
         return accessToken;
     }
 
+
     async getUserByEmail(user: LoginInputDTO) {
 
-        
+        if (!user.email) {
+            throw new Error("Empty 'Email'")
+        } else if (
+            this.emailValidation.isValidEmail(user.email)
+        ) {
+            throw new Error("Invalid 'Email'")
+        }
+
         const userFromDB = await this.userDataBase.getUserByEmail(user.email);
 
-        const hashManager = new HashManager();
-        const hashCompare = await hashManager.compare(user.password, userFromDB.getPassword());
 
-        const authenticator = new Authenticator();
-        const accessToken = authenticator.generateToken({ id: userFromDB.getId(), role: userFromDB.getRole() });
+        if (!user.password) {
+            throw new Error("Empty 'Password'")
+        }
+
+        const hashCompare = await this.hashManager.compare(user.password, userFromDB.getPassword());
+
+        const accessToken = this.authenticator.generateToken({ id: userFromDB.getId(), role: userFromDB.getRole() });
 
         if (!hashCompare) {
             throw new Error("Invalid Password!");
